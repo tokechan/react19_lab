ad966558bee213c4783c48000e9b6dcea56a13eb
toketime <flatoke@gmail.com>
2025-01-27 22:54:11 +0900

    hono install

---
+++ backend/src/index.ts
@@ -0,0 +1,32 @@
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+type BookManager = {
+  id: number;
+  name: string;
+  status: string;
+};
+
+const bookManager: BookManager[] = [
+  { id: 1, name: "React intro", status: "在庫あり" },
+  { id: 2, name: "TypeScript intro", status: "貸出中" },
+  { id: 3, name: "Next.js intro", status: "返却済み" },
+];
+
+app.get("/books", async (c) => {
+  return c.json(bookManager);
+}); 
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+const port = 8080;
+console.log(`Server is running on http://localhost:${port}`)
+
+serve({
+  fetch: app.fetch,
+  port
+})
