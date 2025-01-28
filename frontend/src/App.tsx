5c7cd816077381bc9d64b1246ad9f70db965dbb6
toketime <flatoke@gmail.com>
2025-01-28 09:33:22 +0900

    backend add

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { use } from "react"
+import { BookManage, BookManageJson } from "./domain/book"
+
+async function fetchManageBook() {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  const response = await fetch("http://localhost:8080/books");
+  const data = (await response.json()) as BookManageJson[];
+  return data.map((book) => new BookManage(book.id, book.name, book.status));
+}
+
+const fetchManageBookPromise = fetchManageBook();
+
 
 function App() {
+  const initialBooks = use (fetchManageBookPromise);
+
   return (
     <>
     <div>
-      <button className='bg-blue-500 hover:bg-blue-700 text-green font-bold'>
-        Button
-      </button>
+    <div>
+     <ul>
+      {initialBooks.map((book: BookManage) => {
+        return  <li key={book.id}>{book.name}</li>;
+      })}
+     </ul>
+    </div>
     </div>
     </>
   );
---
+++ frontend/src/domain/book.ts
@@ -0,0 +1,13 @@
+export class BookManage {
+    constructor(
+        public id: number,
+        public name: string,
+        public status: string,
+    ) {}
+}
+
+export type BookManageJson = {
+        id: number;
+        name: string;
+        status: string;
+};
